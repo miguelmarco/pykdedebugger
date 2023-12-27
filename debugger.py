93dbfb6ff2aff3f44d9030688dae2350d9ea387c
Miguel Marco <mmarco@unizar.es>
2023-12-28 00:24:10 +0100

    Resize columns

--- debugger.py
+++ debugger.py
@@ -1,18 +1,18 @@
 #! /usr/bin/python
 
-#A PyQt tool to help in the debugging of Sage.
-#Copyright 2013 Miguel Angel Marco Buzunariz.
+# A PyQt tool to help in the debugging of Sage.
+# Copyright 2013 Miguel Angel Marco Buzunariz.
 
 
-#This program is free software; you can redistribute it and/or
-#modify it under the terms of the GNU General Public License
-#as published by the Free Software Foundation; either version 2
-#of the License, or (at your option) any later version.
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
 
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
 
 import sys
@@ -22,19 +22,19 @@ import time
 
 
 class Debugger(QtWidgets.QDialog):
-    def __init__(self, parent = None):
+    def __init__(self, parent=None):
         QtWidgets.QDialog.__init__(self)
         self.ui = uic.loadUi("debugger.ui")
-        self.history=[]
-        self.nhistory=0
+        self.history = []
+        self.nhistory = 0
         self.ui.show()
 
     def tracecommand(*args):
-        window.history=[]
-        window.nhistory=0
-        konsoleproxy.runCommand('trace(\''+str(window.ui.tracecommand.text())+'\')')
+        window.history = []
+        window.nhistory = 0
+        konsoleproxy.runCommand(
+            'trace(\''+str(window.ui.tracecommand.text())+'\')')
         window.prints()
-
 
     def printn(*args):
         konsoleproxy.runCommand('next')
@@ -56,35 +56,36 @@ class Debugger(QtWidgets.QDialog):
         window.actualize()
 
     def backbutton(*args):
-        window.nhistory-=1
+        window.nhistory -= 1
         window.showhistory()
 
     def forwardbutton(*args):
-        window.nhistory+=1
+        window.nhistory += 1
         window.showhistory()
 
     def showhistory(*args):
-        katefilestring=window.history[window.nhistory][0]
-        katefileline=window.history[window.nhistory][1]
-        kateproxy.openUrl(u"file:/"+katefilestring,"ascii")
-        kateproxy.setCursor(katefileline-1,0)
+        katefilestring = window.history[window.nhistory][0]
+        katefileline = window.history[window.nhistory][1]
+        kateproxy.openUrl(u"file:/"+katefilestring, "ascii")
+        kateproxy.setCursor(katefileline-1, 0)
         window.ui.localslist.setRowCount(0)
-        listparsed=window.history[window.nhistory][2]
+        listparsed = window.history[window.nhistory][2]
         for entry in listparsed:
             rowNum = window.ui.localslist.rowCount()
             window.ui.localslist.insertRow(rowNum)
-            window.ui.localslist.setItem(rowNum, 0, QtWidgets.QTableWidgetItem(entry[0]))
-            window.ui.localslist.setItem(rowNum, 1, QtWidgets.QTableWidgetItem(entry[1]))
-            window.ui.localslist.resizeColumnsToContents()
-        if window.nhistory==0:
+            window.ui.localslist.setItem(
+                rowNum, 0, QtWidgets.QTableWidgetItem(entry[0]))
+            window.ui.localslist.setItem(
+                rowNum, 1, QtWidgets.QTableWidgetItem(entry[1]))
+            # window.ui.localslist.resizeColumnsToContents()
+        if window.nhistory == 0:
             window.ui.backbutton.setDisabled(True)
         else:
             window.ui.backbutton.setEnabled(True)
-        if window.nhistory==len(window.history)-1:
+        if window.nhistory == len(window.history)-1:
             window.ui.forwardbutton.setDisabled(True)
         else:
             window.ui.forwardbutton.setEnabled(True)
-
 
     def actualize(*args):
         tries = 0
@@ -94,39 +95,41 @@ class Debugger(QtWidgets.QDialog):
                 return
             try:
                 time.sleep(0.1)
-                logfile=open(str(window.ui.logfilerequester.text()))
-                logfilelines=logfile.readlines()
-                stringparsed=logfilelines[-8]
-                a=stringparsed.index('(')
-                b=stringparsed.index(')')
-                katefilestring=stringparsed[10:a-4]
-                katefileline=int(stringparsed[a+1:b])
+                logfile = open(str(window.ui.logfilerequester.text()))
+                logfilelines = logfile.readlines()
+                stringparsed = logfilelines[-8]
+                a = stringparsed.index('(')
+                b = stringparsed.index(')')
+                katefilestring = stringparsed[10:a-4]
+                katefileline = int(stringparsed[a+1:b])
                 logfile.close()
                 break
-            except(ValueError):
+            except (ValueError):
                 pass
         konsoleproxy.runCommand('[(a[0],str(a[1])) for a in locals().items()]')
         time.sleep(0.2)
-        logfile=open(str(window.ui.logfilerequester.text()))
-        logfilelines=logfile.readlines()
-        stringparsed=logfilelines[-2]
+        logfile = open(str(window.ui.logfilerequester.text()))
+        logfilelines = logfile.readlines()
+        stringparsed = logfilelines[-2]
         logfile.close()
         try:
-            listparsed=eval(stringparsed)
+            listparsed = eval(stringparsed)
         except:
-            listparsed=[]
+            listparsed = []
         listparsed.sort()
-        window.history.append([katefilestring,katefileline,listparsed])
-        window.nhistory=len(window.history)-1
+        window.history.append([katefilestring, katefileline, listparsed])
+        window.nhistory = len(window.history)-1
         window.showhistory()
-        #konsoleproxy.runCommand('globals()')
+        # konsoleproxy.runCommand('globals()')
+
 
 if __name__ == "__main__":
-    bus=dbus.SessionBus()
-    katebus=list(filter(lambda i: 'kate' in i, [aa.__str__() for aa in bus.list_names()]))[0]
-    konsoleproxy=bus.get_object(katebus,'/Sessions/1')
-    kateproxy=bus.get_object(katebus,'/MainApplication')
-    fileline=None
+    bus = dbus.SessionBus()
+    katebus = list(filter(lambda i: 'kate' in i, [
+                   aa.__str__() for aa in bus.list_names()]))[0]
+    konsoleproxy = bus.get_object(katebus, '/Sessions/1')
+    kateproxy = bus.get_object(katebus, '/MainApplication')
+    fileline = None
     app = QtWidgets.QApplication(sys.argv)
     window = Debugger()
     window.ui.nbutton.clicked.connect(window.printn)
